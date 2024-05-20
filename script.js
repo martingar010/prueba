540fc8c3268c538e292ebd4684bde82af94793b9
martingar010 <168462642+martingar010@users.noreply.github.com>
2024-05-20 13:24:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function playsoun1() {
+  let audio = document.getElementById("audio1");
+  if (audio.paused) audio.play();
+  else audio.pause();
+
+function playsoun2() {
+  let audio = document.getElementById("audio1");
+  if (audio.paused) audio.play();
+  else audio.pause();
+
+function playsoun3() {
+  let audio = document.getElementById("audio1");
+  if (audio.paused) audio.play();
+  else audio.pause();
+
+function playsoun4() {
+  let audio = document.getElementById("audio1");
+  if (audio.paused) audio.play();
+  else audio.pause();
+
+
+function playsoun5() {
+  let audio = document.getElementById("audio1");
+  if (audio.paused) audio.play();
+  else audio.pause();
+
+
+          function playsoun1() {
+  let audio = document.getElementById("audio1");
+  if (audio.paused) audio.play();
+  else audio.pause();
